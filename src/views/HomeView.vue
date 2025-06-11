d63b6303bab36c8c30dd76d3404f9a57cc20aef8
Mateo Galic <mateo.galic@decode.agency>
2025-06-11 08:29:23 +0200

    add prime vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,10 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Button from 'primevue/button'
+</script>
 
 <template>
   <main>
     <h1>Hello from Vue!</h1>
+    <Button>Hello</Button>
   </main>
 </template>
