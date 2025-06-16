12243ae8cf33b75a640250c1f93dbcfa3ba84d4e
Mateo Galic <mateogalic112@gmail.com>
2025-06-16 20:18:12 +0200

    add api keys

---
+++ src/api/auctions/queryKeys.ts
@@ -0,0 +1,7 @@
+const auctionKeys = {
+  all: ['auctions'] as const,
+  lists: () => [...auctionKeys.all, 'list'] as const,
+  list: (limit: number) => [...auctionKeys.lists(), limit] as const,
+  details: () => [...auctionKeys.all, 'detail'] as const,
+  detail: (id: number) => [...auctionKeys.details(), id] as const,
+}
---
+++ src/api/products/queryKeys.ts
@@ -0,0 +1,7 @@
+const productKeys = {
+  all: ['products'] as const,
+  lists: () => [...productKeys.all, 'list'] as const,
+  list: (limit: number) => [...productKeys.lists(), limit] as const,
+  details: () => [...productKeys.all, 'detail'] as const,
+  detail: (id: number) => [...productKeys.details(), id] as const,
+}
