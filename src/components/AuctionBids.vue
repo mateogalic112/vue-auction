c5d1af8ec61d42bfb096a8de9183097a9d11a97c
Mateo Galic <mateogalic112@gmail.com>
2025-06-21 14:48:27 +0200

    add create bid form

--- src/components/AuctionBids.vue
+++ src/components/AuctionBids.vue
@@ -8,14 +8,16 @@ const { data: bids, isLoading, isError, error } = useGetAuctionBids(props.auctio
 </script>
 
 <template>
-  <ul>
+  <ul class="p-4">
     <li v-if="isLoading">Loading...</li>
     <li v-else-if="isError">Error: {{ error?.message }}</li>
 
     <li v-else-if="bids" v-for="(bid, index) in bids.data" :key="bid.id">
       <p
         :class="{
+          'p-2': true,
           'text-green-800 font-bold': index === 0, // e.g. highest bid
+          'border-b border-gray-100': index !== bids.data.length - 1,
         }"
       >
         {{ formattedPrice(bid.amount) }}
--- src/components/AuctionDetails.vue
+++ src/components/AuctionDetails.vue
@@ -1,9 +1,20 @@
 <script setup lang="ts">
 import type { Auction } from '@/models/Auction.model.ts'
 import AuctionBids from '@/components/AuctionBids.vue'
+import { socket } from '@/config/socket'
+
 defineProps<{
   auction: Auction
 }>()
+
+function onSubmit(event: Event) {
+  const formData = new FormData(event.target as HTMLFormElement)
+
+  const auction_id = formData.get('auction_id')
+  const amount = formData.get('amount')
+
+  socket.emit('bids:create', { amount, auction_id })
+}
 </script>
 
 <template>
@@ -20,6 +31,21 @@ defineProps<{
         })
       }}</time></small
     >
+
+    <form
+      @submit.prevent="onSubmit"
+      class="flex flex-col gap-2 items-start p-4 border border-gray-100 my-4"
+    >
+      <input type="hidden" name="auction_id" :value="auction.id" />
+      <label>
+        Amount:
+        <input name="amount" type="number" class="border border-gray-300 rounded-md px-2" />
+      </label>
+
+      <button type="submit" class="bg-blue-500 text-white rounded-md p-2 cursor-pointer">
+        Place Bid
+      </button>
+    </form>
 
     <AuctionBids :auctionId="auction.id" />
   </section>
