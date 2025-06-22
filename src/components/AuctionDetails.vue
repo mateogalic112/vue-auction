51c836940bf49f2d5cb70da7522c8b6720eccb66
Mateo Galic <mateogalic112@gmail.com>
2025-06-22 13:38:26 +0200

    extract bid form

--- src/components/AuctionDetails.vue
+++ src/components/AuctionDetails.vue
@@ -1,20 +1,11 @@
 <script setup lang="ts">
 import type { Auction } from '@/models/Auction.model.ts'
 import AuctionBids from '@/components/AuctionBids.vue'
-import { socket } from '@/config/socket'
+import CreateBidForm from './CreateBidForm.vue'
 
 defineProps<{
   auction: Auction
 }>()
-
-function onSubmit(event: Event) {
-  const formData = new FormData(event.target as HTMLFormElement)
-
-  const auction_id = formData.get('auction_id')
-  const amount = formData.get('amount')
-
-  socket.emit('bids:create', { amount, auction_id })
-}
 </script>
 
 <template>
@@ -32,21 +23,7 @@ function onSubmit(event: Event) {
       }}</time></small
     >
 
-    <form
-      @submit.prevent="onSubmit"
-      class="flex flex-col gap-2 items-start p-4 border border-gray-100 my-4"
-    >
-      <input type="hidden" name="auction_id" :value="auction.id" />
-      <label>
-        Amount:
-        <input name="amount" type="number" class="border border-gray-300 rounded-md px-2" />
-      </label>
-
-      <button type="submit" class="bg-blue-500 text-white rounded-md p-2 cursor-pointer">
-        Place Bid
-      </button>
-    </form>
-
+    <CreateBidForm :auctionId="auction.id" />
     <AuctionBids :auctionId="auction.id" />
   </section>
 </template>
