a4fcfab858ba759e60ab94bff868167def577d5e
Mateo Galic <mateogalic112@gmail.com>
2025-06-23 22:22:42 +0200

    add animations to bid list

--- src/components/AuctionBids.vue
+++ src/components/AuctionBids.vue
@@ -20,16 +20,38 @@ onMounted(() => {
     <li v-if="isLoading">Loading...</li>
     <li v-else-if="isError">Error: {{ error?.message }}</li>
 
-    <li v-else-if="bids" v-for="(bid, index) in bids.data" :key="bid.id">
-      <p
-        :class="{
-          'p-2': true,
-          'text-green-800 font-bold': index === 0, // e.g. highest bid
-          'border-b border-gray-100': index !== bids.data.length - 1,
-        }"
-      >
-        {{ formattedPrice(bid.amount) }}
-      </p>
-    </li>
+    <TransitionGroup name="list" tag="ul" v-else-if="bids">
+      <li v-for="(bid, index) in bids.data" :key="bid.id">
+        <p
+          :class="{
+            'p-2': true,
+            'text-green-800 font-bold': index === 0, // e.g. highest bid
+            'border-b border-gray-100': index !== bids.data.length - 1,
+          }"
+        >
+          {{ formattedPrice(bid.amount) }}
+        </p>
+      </li>
+    </TransitionGroup>
   </ul>
 </template>
+
+<style scoped>
+.list-move, /* apply transition to moving elements */
+.list-enter-active,
+.list-leave-active {
+  transition: all 0.5s ease;
+}
+
+.list-enter-from,
+.list-leave-to {
+  opacity: 0;
+  transform: translateX(30px);
+}
+
+/* ensure leaving items are taken out of layout flow so that moving
+   animations can be calculated correctly. */
+.list-leave-active {
+  position: absolute;
+}
+</style>
